b8c995c3385af296036d5aa47473db63419f777d
Kristono Sugiarto <kristono.sugiarto@gmail.com>
2014-08-05 14:53:54 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PppkTna::Application.config.secret_token = 'b4889af51f1273c10a0e27a5f108f179dda424fd19674b3d710c318b650d2b31ba3286bd50305bc577463395609fcc5dea31afb6d89f2ceb1fc9dc0754d7512b'
